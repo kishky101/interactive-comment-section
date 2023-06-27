0850b21bbd41b00259da847b655a408e731ad50c
Marzuq Adebayo <kishky.101@gmail.com>
2023-06-27 14:14:08 +0200

    clean up my middleware

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -6,11 +6,13 @@ import logger from 'redux-logger'
 
 export type RootState = ReturnType<typeof rootReducer>;
 
+
+
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
 
-    }).concat([logger])
+    }).concat(process.env.NODE_ENV !== 'production'? [logger] : [])
 });
 
 export type AppDispatch = typeof store.dispatch;
